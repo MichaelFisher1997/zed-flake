3b7d83dbee3996bc8a7da3f8076eb06992b15887
MichaelFisher1997 <contact@michaelfisher.tech>
2025-09-11 03:47:57 +0100

    Replace source build with precompiled binary package
    - Switched from building Zed from source to using precompiled binary
    - Simplified flake structure by removing rust-overlay and complex build logic
    - Now uses GitHub releases binary for faster, more reliable builds
    - Removed unused files (README.md, default.nix)
    - Updated to latest Zed version (0.203.4)
    - Added proper app definition for nix run support

--- flake.nix
+++ flake.nix
@@ -1,96 +1,67 @@
 {
-  description = "Zed Editor - High-performance, multiplayer code editor";
+  description = "Zed Editor - A high-performance, multiplayer code editor";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          config = {
-            allowUnfree = true;
-            permittedInsecurePackages = [
-              "openssl-1.1.1w"
-            ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        zed-version = "0.203.4";
+        zed-src = pkgs.fetchurl {
+          url = "https://github.com/zed-industries/zed/releases/download/v${zed-version}/zed-linux-x86_64.tar.gz";
+          sha256 = "0y0p83s1w2r52mjrfc9cvnxhg8shdvyy5j9dq4x22wysgwigndpp";
+        };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "zed";
+          version = zed-version;
+
+          src = zed-src;
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          unpackPhase = ''
+            tar -xzf $src
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp zed.app/bin/zed $out/bin/
+            
+            # Wrap the binary to ensure it can find necessary libraries
+            wrapProgram $out/bin/zed \
+              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc.lib
+                pkgs.xorg.libX11
+                pkgs.xorg.libXcursor
+                pkgs.xorg.libXi
+                pkgs.xorg.libXrandr
+                pkgs.wayland
+                pkgs.libxkbcommon
+                pkgs.vulkan-loader
+              ]} \
+              --set XDG_RUNTIME_DIR "/run/user/$(id -u)"
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A high-performance, multiplayer code editor";
+            homepage = "https://zed.dev";
+            license = licenses.unfree;
+            platforms = [ "x86_64-linux" ];
+            maintainers = [ maintainers.micqdf ];
           };
         };
 
-        # Use our custom build
-        zed = pkgs.callPackage ./default.nix {
-          withGLES = false;
-          profile = "release";
+        apps.zed = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/zed";
         };
-
-        zedGLES = pkgs.callPackage ./default.nix {
-          withGLES = true;
-          profile = "release";
-        };
-
-        zedDebug = pkgs.callPackage ./default.nix {
-          withGLES = false;
-          profile = "dev";
-        };
-      in {
-        packages = {
-          default = zed;
-          inherit zed zedGLES zedDebug;
-        };
-
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = [
-              zed
-              pkgs.rust-analyzer
-              pkgs.cargo
-              pkgs.rustc
-              pkgs.nodejs
-              pkgs.protobuf
-              pkgs.pkg-config
-              pkgs.openssl
-              pkgs.fontconfig
-              pkgs.freetype
-              pkgs.cmake
-              pkgs.makeWrapper
-            ];
-          };
-        };
-
-        apps = {
-          zed = {
-            type = "app";
-            program = "${zed}/bin/zed";
-          };
-          zed-gles = {
-            type = "app";
-            program = "${zedGLES}/bin/zed";
-          };
-          zed-debug = {
-            type = "app";
-            program = "${zedDebug}/bin/zed";
-          };
-        };
-
-        formatter = pkgs.alejandra;
-
-        overlays.default = final: prev: {
-          zed-editor = zed;
-          zed-editor-gles = zedGLES;
-          zed-editor-debug = zedDebug;
-        };
-      }
-    );
+        
+        apps.default = self.apps.${system}.zed;
+      });
 }
